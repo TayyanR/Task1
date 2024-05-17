5b20ae54c78b17c8d2d61603ad2cf15f0bb3441a
teeyyan <140408088+teeyyan@users.noreply.github.com>
2024-05-17 19:11:00 +0400

    taks

---
+++ Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    
+    static bool IsPrime(int num)
+    {
+        if (num < 2) return false;
+        for (int i = 2; i * i <= num; i++)
+            if (num % i == 0) return false;
+        return true;
+    }
+
+    static void Main(string[] args)
+    {
+        // Bütün case-lər yoxlanır
+        for (int num = 1; num <= 30; num++)
+            Console.WriteLine("{0}: {1}", num, IsPrime(num) ? "Sadə ədəddir" : "Mürəkkəb ədəddir");
+    }
+}
